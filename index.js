e2c58859a4bd609d197fb439053686309b4c9ed0
Alena Haurylenka <alena.haurylenka@gmail.com>
2018-10-10 15:51:02 +0300

    Switched to heroku sample app.

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+var favicon = require('serve-favicon')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
